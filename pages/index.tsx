9241197c74f6dfef0d5914c312257eb37bb32527
Renan Garcia <renangarcia@live.com>
2021-06-12 17:38:53 -0300

    chore(): initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Bem vinda, Velha!</h1>
+      <h2>Viva México!</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/KewfYKJy8YU?controls=0&amp;start=49" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  )
+}
+
+export default Home
