66863c65f377f3f9b3ad8c7875686b8a52c88502
Renan Garcia <renangarcia@live.com>
2021-06-12 17:41:11 -0300

    fix(index): fix wrong video name attrs

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h1>Bem vinda, Velha!</h1>
       <h2>Viva México!</h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/KewfYKJy8YU?controls=0&amp;start=49" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/KewfYKJy8YU?controls=0&amp;start=49" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   )
 }
